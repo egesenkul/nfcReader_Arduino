1a17a01d3fd586cea6274b4b07162a35bd57e85f
egesenkul <egesenkul35@hotmail.com>
2018-04-12 19:58:04 +0300

    Added enter and exit status

--- nfcReader/nfcReader.ino
+++ nfcReader/nfcReader.ino
@@ -10,21 +10,63 @@ PN532_SPI pn532spi(SPI, 10);
 SNEP nfc(pn532spi);
 uint8_t ndefBuf[128];
 
+  bool temizlikciOdada = false;
+  bool musteriOdada = false;
+
+bool Odakontrol (String mesaj){
+  //oda numarasını kontrol edecek fonksiyondur
+  if(mesaj.equals("A2")){
+      return true;
+    }
+    return false;
+  }
+
 void Ege(String mesaj){
-    String tarih = mesaj.substring(mesaj.indexOf("TARİHİ:")+9,mesaj.indexOf(" MESAJ"));
-    String icerik = mesaj.substring(mesaj.indexOf("İÇERİĞİ: ")+14,mesaj.length());
-    Serial.println(tarih);
-    Serial.println(icerik);
-    Serial.println();
+    String tarih = mesaj.substring(0,9);
+    String icerik = mesaj.substring(9,mesaj.length());
+    int i=0,j=0,k=0;
+    String array[6]={"","","","","",""};
+    for(i=0;i<icerik.length();i++){
+        if(icerik.charAt(i)==' '){
+            array[k]=icerik.substring(j,i);
+            k++;
+            j=i+1;
+          }
+      }
+      if(Odakontrol(array[0])){
+         if(array[1].equals("TEMIZLIKCI") && !temizlikciOdada){
+          //temizlikçi odaya giris yapsın
+            temizlikciOdada = true;
+            Serial.println(tarih+" "+array[1]+" "+array[2]+" odaya giriş yaptı");
+          }
+          else if (array[1].equals("TEMIZLIKCI") && temizlikciOdada){
+            //temizlikçi odadan çıkış yapsın
+            temizlikciOdada = false;
+            Serial.println(tarih+" "+array[1]+" "+array[2]+" odadan çıkış yaptı");
+            }
+            else if (array[1].equals("MUSTERI") && !musteriOdada){
+            //müşteri odaya giriş yapsın
+            musteriOdada = true;
+            Serial.println(tarih+" "+array[1]+" "+array[2]+" odaya giriş yaptı");
+            }
+            else if (array[1].equals("MUSTERI") && musteriOdada){
+            //müşteri odadan çıkış yapsın
+            musteriOdada = false;
+            Serial.println(tarih+" "+array[1]+" "+array[2]+" odadan çıkış yaptı");
+            }
+      }
+      else{
+        Serial.println("Oda giriş izniniz bulunmuyor.");
+        }
   }
 
 void setup() {
     Serial.begin(9600);
-    Serial.println("EGE ŞENKUL NFC OKUMA ÖRNEK KODUDUR");
+   // Serial.println("EGE ŞENKUL NFC OKUMA ÖRNEK KODUDUR");
 }
 
 void loop() {
-    Serial.println("Cihazı yakınlaştırın");
+    //Serial.println("Cihazı yakınlaştırın");
     int msgSize = nfc.read(ndefBuf, sizeof(ndefBuf));
     if (msgSize > 0) {
         NdefMessage msg  = NdefMessage(ndefBuf, msgSize);
